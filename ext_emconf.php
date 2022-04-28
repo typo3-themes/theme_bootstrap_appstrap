57510669c00139ee3625e9cc2c4229cbf33f0fd0
Kay Strobach <github@kay-strobach.de>
2022-04-28 13:34:44 +0200

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -35,7 +35,6 @@ $EM_CONF[$_EXTKEY] = array(
     'constraints' => array(
         'depends' => array(
             'php' => '5.2.0-0.0.0',
-            'typo3' => '6.2.0-6.2.99',
             'theme_bootstrap' => '1.4.3-1.99.99',
         ),
         'conflicts' => array(),
